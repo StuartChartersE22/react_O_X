53c9a2a7ac3bfbf206c5221cfe27ce314aac39a9
StuartCharters <charters.stuart@gmail.com>
2018-08-29 15:08:00 +0100

    can find winner of the game

---
+++ src/components/Winner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Winner = (props) => {
+  return (
+    <div>
+      <h2>Winner: {props.winner}</h2>
+    </div>
+  );
+}
+
+export default Winner;
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Board from '../components/Board.js';
+import Winner from '../components/Winner.js';
 
 class Game extends React.Component {
   constructor(props) {
@@ -10,27 +11,84 @@ class Game extends React.Component {
         [null, null, null],
         [null, null, null]
       ],
-      turnNum: 0
+      turnNum: 0,
+      winner: null
     }
     this.handleTurn = this.handleTurn.bind(this);
   }
 
-
   handleTurn(tableCoord) {
     const originalGameState = this.state.gameState;
     const coord = tableCoord.split(':');
+    originalGameState[parseInt(coord[1])][parseInt(coord[0])] = this.player();
+    const winner = this.getWinner(originalGameState)
+    this.setState({gameState: originalGameState, turnNum: this.state.turnNum + 1, winner: winner})
+  }
+
+  player() {
     if (this.state.turnNum % 2 === 0){
-      originalGameState[parseInt(coord[1])][parseInt(coord[0])] = 'X'
+      return 'X';
+    }else {
+      return 'O';
     }
-    else {
-      originalGameState[parseInt(coord[1])][parseInt(coord[0])] = 'O'
+  }
+
+  getWinner(board) {
+    let winner = this.checkRows(board);
+    winner = winner || this.checkColumns(board);
+    winner = winner || this.checkFallingDiagonal(board);
+    winner = winner || this.checkLeadingDiagonal(board);
+    return winner;
+  }
+
+  checkRows(board){
+    for(let row of board){
+      const winner = this.checkWin(row);
+      if(winner !== null) return winner;
     }
-    this.setState({gameState: originalGameState, turnNum: this.state.turnNum + 1})
+    return null;
+  }
+
+  checkColumns(board){
+    for (var columnNum = 0; columnNum < board.length; columnNum++) {
+      const column = board.map((row) => {
+        return row[columnNum];
+      });
+      const winner = this.checkWin(column);
+      if(winner !== null) return winner;
+    };
+    return null;
+  }
+
+  checkFallingDiagonal(board){
+    const fallingDiagonal = board.map((row, index) => {
+      return row[index];
+    });
+    return this.checkWin(fallingDiagonal);
+  }
+
+  checkLeadingDiagonal(board){
+    const leadingDiagonal = board.map((row, index) => {
+      return row[board.length - (1+index)];
+    });
+    return this.checkWin(leadingDiagonal);
+  }
+
+  checkWin(array) {
+    const allSame = array.every((cell, index, originalArray) => {
+      if(cell === null) return false;
+      return cell === originalArray[0];
+    })
+    if(allSame) return this.player();
+    return null;
   }
 
   render() {
     return (
-      <Board gameState = {this.state.gameState} onTurn = {this.handleTurn}/>
+      <div>
+        <Board gameState = {this.state.gameState} onTurn = {this.handleTurn}/>
+        <Winner winner={this.state.winner}/>
+      </div>
     );
   }
 
